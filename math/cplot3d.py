c99ec3ed3d32d6ab1e7ec31517ca1da6cdb9dbed
Curiosidad-Racional <hecenra@gmail.com>
2014-07-31 01:08:34 +0200

    primer commit

--- math/cplot3d.py
+++ math/cplot3d.py
@@ -1,19 +1,20 @@
 def cplot3d(expr, xlim = [-1, 1], ylim = [-1, 1], points = 50, style = "real-imag"):
-    # Adaptado de http://ask.sagemath.org/question/9843/3d-complex-function-plot/
+    # Importaciones necesarias
     from mpl_toolkits.mplot3d import Axes3D
     from matplotlib import cm
     import matplotlib.pyplot as plt
     from numpy import arange, array, meshgrid, abs, size, absolute, angle
     from sympy import Symbol, lambdify
 
+    # Para poder representar tanto funciones como expresiones
+    # transformamos las expresiones en funciones.
     if hasattr(expr, 'atoms'):
         var = list(expr.atoms(Symbol))[0]
         Fz=lambdify(var, expr, 'numpy')
     else:
         Fz=expr
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+    # Obtenemos los valores de X, Y, Z
     X = arange(xlim[0], xlim[1], (xlim[1]-xlim[0])/(points-1))
     Y = arange(ylim[0], ylim[1], (ylim[1]-ylim[0])/(points-1))
     X, Y = meshgrid(X, Y)
@@ -42,20 +43,21 @@ def cplot3d(expr, xlim = [-1, 1], ylim = [-1, 1], points = 50, style = "real-ima
         N = (T - T.min())/(T.max() - T.min()) #abs(T/T.max())  # normalize 0..1
         zlabel = "Re(f(z))"
         keyhue = "Im(f(z))"
-    plt.title(' $\mathrm{f(z)}$')
-    ax.set_xlabel(' $\mathrm{Re(z)}$')
-    ax.set_ylabel(' $\mathrm{Im(z)}$')
-    ax.set_zlabel(' $\mathrm{'+zlabel+'}$')
+
+    # Preparamos el plot
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    plt.title('$\mathrm{f(z)}$')
+    ax.set_xlabel('$\mathrm{Re(z)}$')
+    ax.set_ylabel('$\mathrm{Im(z)}$')
+    ax.set_zlabel('$\mathrm{'+zlabel+'}$')
     surf = ax.plot_surface(
         X, Y, Z, rstride=1, cstride=1,
         facecolors=cm.jet(N),
         linewidth=0, antialiased=True, shade=False)
-    # Colorbar see http://stackoverflow.com/a/6601210
+
+    # Mostramos la leyenda
     m = cm.ScalarMappable(cmap=cm.jet, norm=surf.norm)
     m.set_array(T)
     p=plt.colorbar(m)
-    p.set_label(' $\mathrm{'+keyhue+'}$')
-
-    #fig.set_size_inches(14,7) #http://stackoverflow.com/questions/332289/how-do-you-change-the-size-of-figures-drawn-with-matplotlib
-    #plt.show() # if you run it as a python script
-
+    p.set_label('$\mathrm{'+keyhue+'}$')
