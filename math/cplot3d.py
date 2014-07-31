b80ed6c10f6b535236ba572d37af6e0cc17a1476
Curiosidad-Racional <hecenra@gmail.com>
2014-07-31 12:09:26 +0200

    Representar con color o en dos gráficos

--- math/cplot3d.py
+++ math/cplot3d.py
@@ -1,11 +1,29 @@
-def cplot3d(expr, xlim = [-1, 1], ylim = [-1, 1], points = 50, style = "real-imag"):
+def cplot3d(expr, xlim = [-1, 1], ylim = [-1, 1], points = 50, variables = "real-imag", style = "color"):
+"""
+Representa expresiones o funciones complejas.
+
+   expr      - expresión o función a representar.
+   xlim      - valores máximo y mínimo de la 'x', por defecto [-1, 1].
+   ylim      - valores máximo y mínimo de la 'y', por defecto [-1, 1].
+   points    - puntos usados para representar por cada dimensión: en
+               total points**2 puntos.
+   variables - texto que indica las variables que se representarán,
+                  por defecto "real-imag".
+               Opciones: "mod-arg" "real-arg" "arg-real" "real-imag"
+   style     - texto que indica el tipo de representación:
+               "color": un sólo gráfico en el que la segunda variable
+                  es el color.
+               "" u otros: dos gráficos, el primero para la parte real
+                  de la función, el segundo para la parte imaginaria.
+"""
+[sqrt(3)/3, sqrt(3)*exp(-4*I*pi/3)/3, sqrt(3)*exp(-8*I*pi/3)/3]])
     # Importaciones necesarias
     from mpl_toolkits.mplot3d import Axes3D
     from matplotlib import cm
     import matplotlib.pyplot as plt
-    from numpy import arange, array, meshgrid, abs, size, absolute, angle
-    from sympy import Symbol, lambdify
-
+    from numpy import arange, array, meshgrid, size, absolute, angle
+    from sympy import lambdify, Symbol
+    
     # Para poder representar tanto funciones como expresiones
     # transformamos las expresiones en funciones.
     if hasattr(expr, 'atoms'):
@@ -19,43 +37,71 @@ def cplot3d(expr, xlim = [-1, 1], ylim = [-1, 1], points = 50, style = "real-ima
     Y = arange(ylim[0], ylim[1], (ylim[1]-ylim[0])/(points-1))
     X, Y = meshgrid(X, Y)
     R=Fz(X + 1j*Y)
-    if style == "abs-angle":
+
+    # Elegimos variables a representar
+    if variables == "mod-arg":
         Z = absolute(R)
         T = angle(R)
         zlabel = "abs(f(z))"
         keyhue = "arg(f(z))"
-    elif style == "real-angle":
+    elif variables == "real-arg":
         Z = R.real
         T = angle(R)
         zlabel = "Re(f(z))"
         keyhue = "arg(f(z))"
-    elif style == "angle-real":
+    elif variables == "arg-real":
         Z = angle(R)
         T = R.real
         zlabel = "arg(f(z))"
         keyhue = "Re(f(z))"
-    elif style == "real-imag":
+    elif variables == "real-imag":
         Z = R.real
         T = R.imag
         zlabel = "Re(f(z))"
         keyhue = "Im(f(z))"
-    # Normaliza dado que hue toma valores de 0 a 1
-    N = (T - T.min())/(T.max() - T.min())
 
-    # Preparamos el plot
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    plt.title('$\mathrm{f(z)}$')
-    ax.set_xlabel('$\mathrm{Re(z)}$')
-    ax.set_ylabel('$\mathrm{Im(z)}$')
-    ax.set_zlabel('$\mathrm{'+zlabel+'}$')
-    surf = ax.plot_surface(
-        X, Y, Z, rstride=1, cstride=1,
-        facecolors=cm.jet(N),
-        linewidth=0, antialiased=True, shade=False)
 
-    # Mostramos la leyenda
-    m = cm.ScalarMappable(cmap=cm.jet, norm=surf.norm)
-    m.set_array(T)
-    p=plt.colorbar(m)
-    p.set_label('$\mathrm{'+keyhue+'}$')
+    # Elegimos el tipo de representación
+    if style == "color":
+        # Normaliza dado que hue toma valores de 0 a 1
+        N = (T - T.min())/(T.max() - T.min())
+
+        # Preparamos el plot
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        plt.title('$\mathrm{f(z)}$')
+        ax.set_xlabel('$\mathrm{Re(z)}$')
+        ax.set_ylabel('$\mathrm{Im(z)}$')
+        ax.set_zlabel('$\mathrm{'+zlabel+'}$')
+        surf = ax.plot_surface(
+            X, Y, Z, rstride=1, cstride=1,
+            facecolors=cm.jet(N),
+            linewidth=0, antialiased=True, shade=False)
+
+        # Mostramos la leyenda
+        m = cm.ScalarMappable(cmap=cm.jet, norm=surf.norm)
+        m.set_array(T)
+        p=plt.colorbar(m)
+        p.set_label('$\mathrm{'+keyhue+'}$')
+    else:
+        # Preparamos el primer plot
+        fig = plt.figure(figsize=plt.figaspect(0.5))
+        plt.axis('off')
+        plt.title('$\mathrm{f(z)}$')
+        ax = fig.add_subplot(1, 2, 1, projection='3d')
+        ax.set_xlabel('$\mathrm{Re(z)}$')
+        ax.set_ylabel('$\mathrm{Im(z)}$')
+        ax.set_zlabel('$\mathrm{'+zlabel+'}$')
+        surf = ax.plot_surface(
+            X, Y, Z, rstride=1, cstride=1,
+            cmap=cm.jet,
+            linewidth=0, antialiased=False, shade=False)
+        # Preparamos el segundo plot
+        ax = fig.add_subplot(1, 2, 2, projection='3d')
+        ax.set_xlabel('$\mathrm{Re(z)}$')
+        ax.set_ylabel('$\mathrm{Im(z)}$')
+        ax.set_zlabel('$\mathrm{'+keyhue+'}$')
+        surf = ax.plot_surface(
+            X, Y, T, rstride=1, cstride=1,
+            cmap=cm.jet,
+            linewidth=0, antialiased=False, shade=False)
